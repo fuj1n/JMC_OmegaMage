14df551be11bbc5d6acff5a22481fe3261d385be
Arthur Uzulin <fuj1nman@hotmail.com>
2018-09-29 00:19:14 +1000

    Added fire attach spell

---
+++ Project/Assets/Scripts/Spells/SpellAttach.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class SpellAttach : SpellBase
+{
+    public float damagePerSecond = 5F;
+
+    public float duration = 4F;
+    public float durationVariance = 0.5F;
+
+    public bool fadeAnimation = true;
+    [ConditionalHide(true, ConditionalSourceField = "fadeAnimation")]
+    public float fadeTime = 1F;
+
+    private float startTime;
+
+    private Transform target;
+
+    private void Start()
+    {
+        startTime = Time.time;
+        duration = Random.Range(duration - durationVariance, duration + durationVariance);
+    }
+
+    private void Update()
+    {
+        transform.position = target.position;
+
+        float elapsed = Mathf.Clamp01((Time.time - startTime) / duration);
+
+        if (fadeAnimation)
+        {
+            float fadeAt = 1 - (fadeTime / duration);
+            if (elapsed > fadeAt)
+            {
+                float fadePercent = Mathf.Clamp01((elapsed - fadeAt) / (1 - fadeAt));
+                Vector3 position = transform.position;
+                position.z = fadePercent * 2;
+                transform.position = position;
+            }
+        }
+
+        if (elapsed >= 1F)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        OnTriggerStay(collision.collider);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        IEnemy recepient = other.GetComponent<IEnemy>();
+        if (recepient != null)
+        {
+            if (damagePerSecond != 0F)
+                recepient.TakeDamage(damagePerSecond, element, true);
+        }
+    }
+
+    public override void Cast(ISpellParams parameters)
+    {
+        SpellTargetParams targetParams = (SpellTargetParams)parameters;
+
+        Instantiate(gameObject).GetComponent<SpellAttach>().target = targetParams.destination;
+    }
+
+    public override string GetTargetType() => "Enemy";
+}
