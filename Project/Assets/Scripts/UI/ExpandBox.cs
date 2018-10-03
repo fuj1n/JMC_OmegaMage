ff775618186beb48d0e521de028f6c2b39f68457
Arthur Uzulin (fuj1n) <fuj1nman@hotmail.com>
2018-10-03 13:57:17 +1000

    Added an ability to minimize the spell descriptions

---
+++ Project/Assets/Scripts/UI/ExpandBox.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExpandBox : MonoBehaviour, IPointerClickHandler
+{
+    [Header("State")]
+    public bool isExpanded = true;
+    public float speed = 1F;
+
+    [Header("Positions")]
+    public Vector2 expanded = Vector2.zero;
+    public Vector2 retracted = Vector2.zero;
+    public float expandedArrowAngle = 180F;
+    public float retractedArrowAngle = 0F;
+
+    private Tween internalTween;
+    private new RectTransform transform;
+    private RectTransform arrow;
+
+    private void Awake()
+    {
+        UpdateState();
+        transform = GetComponent<RectTransform>();
+        arrow = transform.Find("Arrow").GetComponent<RectTransform>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (internalTween != null && internalTween.IsPlaying())
+            return;
+
+        isExpanded = !isExpanded;
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        Vector2 target = isExpanded ? expanded : retracted;
+        internalTween = transform.DOAnchorPos(target, speed);
+
+        if (arrow)
+        {
+            float angle = isExpanded ? expandedArrowAngle : retractedArrowAngle;
+            arrow.DORotate(Vector3.forward * angle, speed);
+        }
+    }
+}
